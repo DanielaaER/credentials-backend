27d5a47578e0775af468d0a177113ccabd98cbc6
Daniela Espinosa <daniela.espinosa.3103@gmail.com>
2025-04-12 16:00:00 -0600

    fix

--- repositories/ingreso.py
+++ repositories/ingreso.py
@@ -13,7 +13,7 @@ from models.users.estudiante import estudiantes
 from models.ingreso import ingreso
 from models.institucion.clases import clases
 from models.users.users import users
-from models.institucion.horarios import horarios
+from models.horario import horarios
 from models.institucion.aulas import aulas
 
 Session = sessionmaker(bind=engine)
